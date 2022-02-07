632e95996cf7b266b408c61430c667ba3b2626e6
d33zhou <d33zhou@outlook.com>
2022-02-07 01:20:40 +0000

    add user authentication via bcrypt, add new User model/controller/views and Session controller/views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # --- cart methods ---
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
@@ -20,13 +21,22 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  # --- user authentication methods ---
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
   end
 
 end
